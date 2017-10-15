6933e4250830c493f7e6de4dd0ee50f301ae950b
jauer45 <joeauer@gmx.com>
2017-10-15 06:24:56 +0100

    Change composition of index.js

--- scribblr/index.js
+++ scribblr/index.js
@@ -1,40 +1,42 @@
 var scribble = require('scribbletune');
 
-var hh = scribble.clip({
-	notes:     'c#5',
-	pattern:   'x___x_xxx___x___',
-	accentMap: 'x_______x_______'
+
+var mvA  = scribble.clip({
+	notes:     'FMaj#4 BMaj#4 EMaj#4 AMaj#4 DMaj#4 GMaj#4 Cmb4 Fmb4',
+	pattern:   'x_x_x_x_x_x_x_x_',
+	accentMap: 'x___x___x___x___'
 });
 
-var bd = scribble.clip({
-	notes:     'c#3',
-	pattern:   'x___'.repeat(4),
-	accentMap: 'x___'.repeat(4)
+var mvB = scribble.clip({
+	notes:     'BMaj#4 EMaj#4 AMaj#4 DMaj#4 GMaj#4 Cmb4 Fmb4 Bmb4',
+	pattern:   'x_x_x___x_x_x_x_',
+	accentMap: 'x___x___x_______'
 });
 
-var tm = scribble.clip({
-	notes:     'f#3',
-	pattern:   'x_x___x___x_x___',
-	accentMap: 'x_x_____x_x___x_'
+var mvC = scribble.clip({
+	notes:     'EMaj#4 AMaj#4 DMaj#4 GMaj#4 Cmb4 Fmb4 Bmb4 Emb4',
+	pattern:   'x_x_x_x_x_x_x_x_',
+	accentMap: 'x___x___x___x___'
 });
 
-var bs = scribble.clip({
-	notes:     'g#2',
-	pattern:   'x_x_____x____x__',
-	accentMap: 'x_______x_______'
+var mvD = scribble.clip({
+	notes:     'AMaj#4 DMaj#4 GMaj#4 Cmb4 Fmb4 Bmb4 Em4 Am4',
+	pattern:   'x_x_x___x_x_x_x_',
+	accentMap: 'x___x___x_______'
 });
 
-var chrd = scribble.clip({
+var mvE = scribble.clip({
+        notes:     'DMaj#4 GMaj#4 Cmb4 Fmb4 Bmb4 Em4 Am4 Dm4',
+        pattern:   'x_x_x_x_x_x_x_x_',
+        accentMap: 'x___x___x___x___'
+});
+
+var mv = scribble.clip({
 	notes: 'CMaj#4 FMaj#4 BMaj#4 EMaj#4 AMaj#4 DMaj#4 GMaj#4 Cmb4',
 	pattern:   'x_x_x_x_x_x_x_x_',
-	accentMap: 'x_x_____x_x___x_'
+	accentMap: 'x___x___x_______'
 });
 
+
 // Render a MIDI file of this clip
-scribble.midi(chrd, 'chord.mid');
-scribble.midi(hh, 'hh.mid');
-scribble.midi(bd, 'bd.mid');
-scribble.midi(tm, 'tm.mid');
-scribble.midi(bs, 'bs.mid');
-
-
+scribble.midi( mv.concat( mvA.concat( mvB.concat( mvC.concat( mvD.concat( mvE, 'TrackOne.mid') ) ) ) ) );
